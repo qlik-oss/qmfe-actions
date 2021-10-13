775b10015ff2a88a951e384c701b0e154218d8dd
Andreas Jonsson <aoj@qlik.com>
2021-10-13 14:23:51 +0200

    export locked var

--- import-map-to-aws/entrypoint.sh
+++ import-map-to-aws/entrypoint.sh
@@ -158,7 +158,7 @@ deploy_import_map() {
   # Upload import-map to default folder if file is NOT locked
   if [[ "$locked" == "\"true\"" ]]; then
     echo "$aws_s3_bucket_import_map_url is locked - new version of import-map could not be applied."
-    export LOCKED=true
+    echo "locked=true" >> "$GITHUB_ENV"
   else
     echo "Deploy import-map to $aws_s3_bucket_import_map_url"
     aws s3 cp deploy-import-map.json "$aws_s3_bucket_import_map_url" --cache-control "$CACHE_CONTROL" --profile $aws_profile --region $AWS_REGION $dryrun
